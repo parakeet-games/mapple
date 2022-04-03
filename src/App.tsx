0e2f31e17655a5f46552e637d285d67ea4e7a5a3
WriterArtistCoder <tinystripz@outlook.com>
2022-04-03 16:39:33 -0700

    Added GameView and continent map

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import GameView from './GameView';
+import { ReactComponent as Continents } from './resource/continents-map.svg';
 
 function App() {
+  const [gameStarted, setGameStarted] = useState(false)
+
+  if (gameStarted) {
+    return <GameView />;
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <b> Yo baby,
-          Edit <code>src/App.tsx</code> and save to reload.
-        </b>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Continents className='continents-map'></Continents>
+
+        <h1>MAPPLE</h1>
+        <button onClick={() => {
+          setGameStarted(true)
+        }}>GO!</button>
       </header>
     </div>
   );
---
+++ src/GameView.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './GameView.css';
+
+function GameView() {
+  return (
+    <div className="GameView">
+      <header className="GameView-header">
+        {/* <img src={logo} className="GameView-logo" alt="logo" /> */}
+        <h1>That's game, folks</h1>
+      </header>
+    </div>
+  );
+}
+
+export default GameView;
