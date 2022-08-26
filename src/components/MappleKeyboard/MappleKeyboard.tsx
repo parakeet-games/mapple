11c07c000cc4a8686e12fd5f3f4c5c1206975618
Samuel Sharp <samuelsharp4@gmail.com>
2022-08-26 09:48:00 -0700

    Hotfix keyboard

--- src/components/MappleKeyboard/MappleKeyboard.tsx
+++ src/components/MappleKeyboard/MappleKeyboard.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const MappleKeyboard = (props: Props)=> {
     const {onChange, onKeyPress} = props;
-
+    // TODO: Backspace causes issues with fullInput, replaced by clear for now
     return (
       <Keyboard
         onChange={onChange}
@@ -17,14 +17,14 @@ export const MappleKeyboard = (props: Props)=> {
               'Q W E R T Y U I O P',
               'A S D F G H J K L',
               'Z X C V B N M',
-              '{bksp} {clear} {space} {enter}'
+              '{clear} {space} {enter}'
             ]
           }}
           display={{
             '{bksp}': '⌫',
             '{clear}': "Clear",
             '{space}': 'Space',
-            '{enter}': '↵'
+            '{enter}': 'Submit'
           }}
       />
     );
