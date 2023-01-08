f78cf8a6aaa786c10928d8fbc3f8b4a64a195284
Samuel Sharp <samuelsharp4@gmail.com>
2023-01-08 12:40:16 -0800

    Update challenge

--- src/GameView.tsx
+++ src/GameView.tsx
@@ -1,37 +1,55 @@
-import React, { useState } from 'react';
-import './GameView.css';
+import React, { useState } from "react";
+import "./GameView.css";
 
-import { GameViewBoxes } from './components/GameViewBoxes/GameViewBoxes';
-import { ReactComponent as Continents } from './resource/continents-map.svg';
+import { GameViewBoxes } from "./components/GameViewBoxes/GameViewBoxes";
+import { ReactComponent as Continents } from "./resource/continents-map.svg";
 
 function GameView() {
-	console.log('%crerender GameView', 'color: blue')
-	const [guesses, setGuesses] = useState<string[]>([])
-	const updateGss = (newGuesses: string[]) => {
-		setGuesses(newGuesses)
-	}
+  console.log("%crerender GameView", "color: blue");
+  const [guesses, setGuesses] = useState<string[]>([]);
+  const updateGss = (newGuesses: string[]) => {
+    setGuesses(newGuesses);
+  };
 
-	const [current, setCurrent] = useState("");
+  const [current, setCurrent] = useState("");
 
-	var test1 = {
-		name: 'Philippines',
-		hints: ['Collection of islands with a tropical marine climate.', 'Inside the aastride typhoon belt', 'Made up of 7,641 islands', 'Colonized by the Spanish', 'Flag is red, blue, and white']
-	};
+  var test1 = {
+    name: "Egypt",
+    hints: [
+      "This country is located in the northeastern region of Africa.",
+      "The official language is a Semitic language that uses the Arabic script.",
+      "The currency is the pound.",
+      "The capital city is home to one of the largest and busiest squares in Africa.",
+      "It is home to some of the oldest and most famous pyramids in the world.",
+    ],
+  };
 
-	return (
-		<>
-			<Continents style={{ width: '100vw', height: '100vh', position: 'fixed', opacity: 0.2, zIndex: -10000 }} />
-			<div className="GameView">
-				<header className="GameView-header">
-					<h1>MAPPLE</h1>
-				</header>
-				<GameViewBoxes current={current}
-					setCurrent={setCurrent}
-					guesses={guesses}
-					setGuesses={updateGss} hints={test1.hints} answer={test1.name} />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Continents
+        style={{
+          width: "100vw",
+          height: "100vh",
+          position: "fixed",
+          opacity: 0.2,
+          zIndex: -10000,
+        }}
+      />
+      <div className="GameView">
+        <header className="GameView-header">
+          <h1>MAPPLE</h1>
+        </header>
+        <GameViewBoxes
+          current={current}
+          setCurrent={setCurrent}
+          guesses={guesses}
+          setGuesses={updateGss}
+          hints={test1.hints}
+          answer={test1.name}
+        />
+      </div>
+    </>
+  );
 }
 
 export default GameView;
