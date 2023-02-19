0b4ab6ca2a621eb03e6fe17c42f3a1b7bd2e7d3a
WriterArtistCoder <tinystripz@outlook.com>
2023-02-18 18:17:33 -0800

    'npm run addc' now works!

--- src/scripts/addChallenge.js
+++ src/scripts/addChallenge.js
@@ -1,27 +1,77 @@
-// `npm run addc` adds a new challenge
-let challenges = require('../resource/challenges.json')
-let countries = require('../resource/countryList.json')
+// `npm run addc` runs this script
+const chaljson = require('../resource/challenges.json')
+const countries = require('../resource/countryList.json')
+
+const fs = require('fs');
+
+let chaltxt = fs.readFileSync('./src/resource/challenges.json').toString();
+let chalcrop = chaltxt.replace(/([\s\S]+)}\s*}\s*/, '$1') // Crop off final closing bracket and whitespace
 
 let indexes = []
-let chalKeys = Object.keys(challenges).sort();
-let latestChal = strToDate(chalKeys[chalKeys.length-1])
+
+// ****
+// NOTE THAT program "thinks" today is the latest date in the chaljson, which may be in the future
+// Does not care about what day "today" actually is
+
+// Relies on the fact that all objects are valid chaljson
+// except for the first, "0default", the default challenge
+
+let chalDates = Object.keys(chaljson).sort();
+chalDates.splice(0, 1); // Removes "0default"
+let latestDate = strToDate(chalDates[chalDates.length-1])
+let over195 = chalDates.length > 195;
+
+let chalAnswers = []
+
+for (var i = 0; i < chalDates.length; i++) {
+    let answer = chaljson[chalDates[i]]['answer']
+    chalAnswers.push(chaljson[chalDates[i]]['answer'])
+
+    if (!over195 // If within the first 195 chaljson, don't toelrate repeats
+        || i >= chalDates.length-30 // Else, don't tolerate repeats from the previous 30
+        ) {
+        countries.splice(countries.indexOf(answer), 1)
+    }
+}
+
+console.log(`Most recent challenge:\n#${chalDates.length} - ${chalDates[chalDates.length-1]} - ${chalAnswers[chalDates.length-1]}`)
 
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-readline.question('How many countries? ', num => {
-    let date = latestChal
+readline.question('\nHow many countries? ', num => {
+    let date = latestDate
+    chalcrop += '}'
+
     for (var i = 0; i < num; i++) {
+        chalcrop += ',\n'
         date.setDate(date.getDate() + 1)
 
         let index = randInt(0, countries.length - 1)
         indexes.push(index)
         
-        console.log(dateToStr(date) + ' ' + countries[index])
+        // console.log(dateToStr(date) + ' ' + countries[index])
+        chalcrop +=
+`  "${dateToStr(date)}": {
+     "answer": "${countries[index]}",
+     "clues": []
+  }`
     }
-    readline.close();
+    chalcrop += '\n}'
+
+    let chaltxt = fs.readFileSync('./src/resource/challenges.json').toString();
+    fs.writeFileSync('./src/resource/challenges.json', chalcrop, {flag:'rs+'});
+
+    console.log(`\nDONE: ${num} new countries added to challenges.json`)
+    if (over195) {
+        console.log(`No conflicts with previous 30 challenges (${chalDates[chalDates.length-30]} - ${chalDates[chalDates.length-1]})`)
+    } else {
+        console.log(`No conflicts with previous challenges`)
+    }
+
+    readline.close()
 });
 
 // Inclusive
